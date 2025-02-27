dc816f721c870456b3ee43452ddfb21a49f6e113
ifef228 <ifef228@gmail.com>
2025-02-27 10:34:33 +0300

    lab1

--- sql_scripts/lab3/notable.sql
+++
@@ -1,8 +0,0 @@
---14
-select * from "Customers";
-
---15
-insert into product(prname) values ('test role');
-
---16
-insert into product(prname) values ('test role')
--- sql_scripts/lab3/user1.sql
+++
@@ -1,7 +0,0 @@
---11
-create database users;
-drop database users;
-
---13
-create database users;
-drop database users;
